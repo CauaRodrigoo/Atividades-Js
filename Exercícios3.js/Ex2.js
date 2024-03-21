7443fb9a9053fda21068edef7992f8d0445c68c0
CauaRodrigoo <cauarodrigoalmeidasilva@gmail.com>
2024-03-21 16:36:19 -0400

    new comit

--- Exercícios3.js/Ex2.js
+++ Exercícios3.js/Ex2.js
@@ -1,10 +1,26 @@
-import entradaDados from "readline-sync";
-/*02. Escreva um programa que tem por finalidade calcular a Média Geral de uma Turma de
-alunos.
-O Programa deverá:
-Ler o número de alunos da turma;
-Ler as notas dos 4 bimestres de cada aluno;
-Calcular e imprimir a média do aluno;
-Calcular e imprimir a média da turma.*/
-let numAlunos = entradaDados.questionInt("Informe um numero de alunos da sala: ");
+import entraDados from "readline-sync"
 
+let num_alunos = entraDados.questionFloat("Digite o numero de alunos")
+let cont_alunos = 0
+let aluno_nota = 0
+let alunomf=0
+let alunonat=1
+
+while (cont_alunos<num_alunos) {
+    while (alunonat<5){
+        let notaa = entraDados.questionFloat(`Digite a ${alunonat} nota do ${cont_alunos+1} aluno:`)
+            aluno_nota+=notaa
+            alunonat++
+        
+
+    }
+    console.log(`Média do ${cont_alunos+1} aluno: ${aluno_nota/4}`)
+
+    alunomf=(aluno_nota/4)+alunomf
+    alunonat=1
+    aluno_nota=0
+    cont_alunos++
+}
+
+
+console.log(`Média da turma: ${alunomf/num_alunos}`)
